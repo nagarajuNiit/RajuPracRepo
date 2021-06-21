32472ebedf5357236d1c0a5d897f1bb61237749c
TL <TL@gmail.com>
2021-06-21 16:03:30 +0530

    adding mail method to TestClass

--- DemoProject/src/com/test/TestClass.java
+++ DemoProject/src/com/test/TestClass.java
@@ -1,5 +1,16 @@
 package com.test;
 
+import com.dss.Utility;
+
 public class TestClass {
 
+	public static void main(String [] args)
+	{
+		
+		Utility utility=new Utility();
+		System.out.println(utility.add(10, 20));
+		
+		
+	}
+	
 }
